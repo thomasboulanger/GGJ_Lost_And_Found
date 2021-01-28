d4dc0ab61f04810eb4a02bb0aad8b3cf9d01d438
thomasboulanger <60351261+thomasboulanger@users.noreply.github.com>
2021-01-28 21:19:10 +0100

    debut

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] 
+    private Vector2 offset;
+    [SerializeField] 
+    private Transform target;
+    [SerializeField] 
+    private float translateSpeed;
+
+    private void Update()
+    {
+        HandleTranslation();
+    }
+
+    private void HandleTranslation()
+    {
+        Vector2 targetPosition = target.TransformPoint(offset);
+        transform.position = Vector2.Lerp(transform.position, targetPosition, translateSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+
+    private Rigidbody2D rb;
+    private float horizontal;
+    private float vertical;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 move = new Vector2(horizontal, vertical);
+        if (Mathf.Abs(horizontal) > 0 && Mathf.Abs(vertical) > 0)
+        {
+            move = .7f * move;
+        }
+        rb.AddForce(move * speed * Time.deltaTime);
+
+
+    }
+}
