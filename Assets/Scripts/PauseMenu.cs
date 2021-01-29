7dd6d89f9bcdcad933545c2a93cae65b3fe7686b
thomasboulanger <60351261+thomasboulanger@users.noreply.github.com>
2021-01-29 18:31:38 +0100

    pause menu et ui in game

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pausePanel;
+
+    private bool isPaused;
+
+    void Start()
+    {
+        isPaused = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        pausePanel.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        pausePanel.SetActive(false);
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
