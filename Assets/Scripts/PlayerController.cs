f64f651fe44cecc46562fe797dd77b2072361c57
Jordan <finalzeroxx@gmail.com>
2021-01-29 10:38:20 +0100

    Push player move

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,12 +25,14 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         Vector2 move = new Vector2(horizontal, vertical);
-        if (Mathf.Abs(horizontal) > 0 && Mathf.Abs(vertical) > 0)
+
+        if (Mathf.Abs(horizontal) > 0 || Mathf.Abs(vertical) > 0)
         {
             move = .7f * move;
+
+            rb.velocity = move * speed;
         }
-        rb.AddForce(move * speed * Time.deltaTime);
 
-
+        //rb.AddForce(move * speed * Time.deltaTime);
     }
 }
