4b954bc01ec8fe549cf280d263388b46e9bc8daa
thomasboulanger <60351261+thomasboulanger@users.noreply.github.com>
2021-01-30 15:58:07 +0100

    beacon

---
+++ Assets/Scripts/ToggleBeacon.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleBeacon : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject beaconOn;
+
+    private float timer;
+    private bool on;
+
+    private void Start()
+    {
+        timer = 1f;
+        on = false;
+    }
+    void Update()
+    {
+        if (on)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        }
+
+        if (timer <= 0)
+        {
+            on = true;
+        }
+        if (timer >=1f)
+        {
+            on = false;
+        }
+        beaconOn.SetActive(on);
+    }
+}
